1739ce3140c94550822adcfd37a6cbbf26cdf602
Lizaveta <lizaveta.dubok@gmail.com>
2022-07-22 20:07:39 +0200

    hello

---
+++ web/hello.py
@@ -0,0 +1,6 @@
+def hello_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    body = QueryString(environ).split('&')
+    return body
